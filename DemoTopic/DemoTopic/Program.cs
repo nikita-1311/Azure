516f00009fae90a74d4fba873c856dc387708a0b
nikita-1311 <nikita-1311>
2022-11-08 12:21:41 +0530

    azure

---
+++ DemoTopic/DemoTopic/Program.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+
+ServiceBusClient client;
+ServiceBusSender sender;
+
+const int no_Of_Messages = 3;
+
+var clientOptions = new ServiceBusClientOptions
+{
+    TransportType = ServiceBusTransportType.AmqpWebSockets
+};
+
+//Here we are using Shared Access Key 
+client = new ServiceBusClient("Endpoint=sb://nikitacakeshop.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=1gNaDlSGoKkgKIKQTp+z7WyPWanqe/S6Moa2ZpsaUvE= ", clientOptions);
+sender = client.CreateSender("mytopic");
+
+//creating a Batch 
+using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+for (int i = 1; i <= no_Of_Messages; i++)
+
+{
+    //try adding a message to the batch 
+    if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+    {
+        throw new Exception($" The Message is too large  to fit in the batch ");
+    }
+}
+try
+{ 
+    await sender.SendMessagesAsync(messageBatch);
+    Console.WriteLine($"A Batch of {no_Of_Messages} has been published to the Queue");
+}
+finally
+{
+    //Need to Cleanup object and Resources in the network 
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+Console.WriteLine("Press an key to close the Appplication");
+Console.ReadKey();
---
+++ DemoTopic/RecievingMessage/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+ServiceBusClient client;
+ServiceBusProcessor processor;
+
+var clientOptions = new ServiceBusClientOptions
+
+{
+    TransportType = ServiceBusTransportType.AmqpWebSockets
+};
+
+client = new ServiceBusClient("Endpoint=sb://nikitacakeshop.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=1gNaDlSGoKkgKIKQTp+z7WyPWanqe/S6Moa2ZpsaUvE= ", clientOptions);
+processor = client.CreateProcessor("mytopic","subscription2", new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+    await processor.StartProcessingAsync();
+    Console.WriteLine("Wait for a minuteand then press key to end the processing...!!!");
+    Console.ReadKey();
+    Console.WriteLine("\n Stopping the reciever ...!!!");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped recieving message .!!!!");
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    string body = args.Message.Body.ToString();
+    System.Console.WriteLine($"Recieved:{body}");
+    await args.CompleteMessageAsync(args.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
