9d055a36d089340e805a5a49e09838cb37f5b74f
nikita-1311 <nikita-1311>
2022-11-12 16:39:10 +0530

    azure

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Azure.Cosmos;
+// New instance of CosmosClient class
+using CosmosClient client = new(
+    accountEndpoint: Environment.GetEnvironmentVariable("COSMOS_ENDPOINT")!,
+    authKeyOrResourceToken: Environment.GetEnvironmentVariable("COSMOS_KEY")!
+);
+// Database reference with creation if it does not already exist
+Database database = await client.CreateDatabaseIfNotExistsAsync(
+    id: "cosmicworks"
+);
+
+Console.WriteLine($"New database:\t{database.Id}");
